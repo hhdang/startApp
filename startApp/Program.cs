92bbcba970a842f2610648ad6feaa0909b5d25e4
hhdang <hhdang@qq.com>
2016-08-23 09:53:57 +0800

    v20160823

--- startApp/Program.cs
+++ startApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace startApp
 {
@@ -10,6 +11,22 @@ namespace startApp
     {
         static void Main(string[] args)
         {
+            string ArrayProcessName = "InRun";
+            string[] processName = ArrayProcessName.Split(',');
+
+            foreach( string appName in processName ){
+                Process[] localByNameApp = Process.GetProcessesByName( appName );
+
+                if( localByNameApp.Length > 0 ){
+                    foreach( Process app in localByNameApp ){
+                        if( !app.HasExited ){
+                            System.Console.WriteLine( app.ProcessName + " exsiting." );
+                        }else{
+                            System.Console.WriteLine(app.ProcessName + " has exited.");
+                        }
+                    }
+                }
+            }
         }
     }
 }
